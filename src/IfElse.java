10df64be3fb2a2edb9cc4fa82dea345f251ab9d0
nahid <eng.yanahid@gmail.com>
2023-03-03 03:14:50 +0600

    If else

---
+++ src/IfElse.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+
+
+public class IfElse {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(bufferedReader.readLine().trim());
+
+        if (N%2 != 0){
+            System.out.println("Weird");
+        }
+        else if (N%2 == 0 && 2<=N && N<=5){
+            System.out.println("Not Weird");
+        }
+        else if (N%2 == 0 && 6<=N && N<=20){
+            System.out.println("Weird");
+        }
+        else if (N%2 == 0 && 20<N){
+            System.out.println("Not Weird");
+        }
+
+        bufferedReader.close();
+    }
+}
